f79a75f6433ad6261faad70d553f0daf0e0038b6
swati ubnare <swatiubnare2@gmail.com>
2020-12-17 22:19:10 +0530

    css change test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-
 import {
   FaEnvelopeOpen,
   FaUser,
@@ -8,21 +7,19 @@ import {
   FaPhone,
   FaLock,
 } from 'react-icons/fa'
-
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
-
 function App() {
   return (
-    <div>
-      <h1>heading 1</h1>
-      <h2>heading 2</h2>
-      <h3>heading 2</h3>
-      <h4>heading 2</h4>
-      <h5>heading 2</h5>
-      <h6>heading 2</h6>
+    <div class='block'>
+      <h1>heading </h1>
+      <h2>heading </h2>
+      <h3>heading </h3>
+      <h4>heading </h4>
+      <h5>heading </h5>
+      <h6>heading </h6>
     </div>
-    );
+  );
 }
 
 export default App
