29cfbd555c849defdc992dfe419b713fad39a0c8
swati ubnare <swatiubnare2@gmail.com>
2020-12-17 21:22:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa'
+
+const url = 'https://randomuser.me/api/'
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+
+function App() {
+  return (
+    <div>
+      <h1>heading 1</h1>
+      <h2>heading 2</h2>
+      <h3>heading 2</h3>
+      <h4>heading 2</h4>
+      <h5>heading 2</h5>
+      <h6>heading 2</h6>
+    </div>
+    );
+}
+
+export default App
